c0469b78df591c330dfaa5d4acd45c44b57a5e21
KalleKryp <harrykarlarvid@gmail.com>
2021-01-19 10:20:33 +0100

    Initial commit

---
+++ My1stProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace My1stProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heltal = 10;
+
+            for (int i = 0; i < heltal; i++)
+            {
+                Console.WriteLine(heltal * i);
+            }
+        }
+    }
+}
